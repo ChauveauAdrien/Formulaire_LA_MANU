129e9a10822d4cdcfbaa8f7f2a5115fcd1fee2c1
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-02-09 17:39:18 +0100

    Création du header

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Manu Formulaire</title>
+    <link rel="stylesheet" href="./assets/css/style.css?version=<?= time(); ?>">
+</head>
+<body>
+    <header>
+        <div class="header-wrapper">
+            <nav class="header-nav">
+                <a href="#"><img src="./assets/img/LaManuUltraCompact_RVB_white.svg" alt="Logo de La Manu" class="header-logo"></a>
+                <a href="#"><div class="burger">
+                    <div class="mid-line"></div>
+                </div></a>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
+
+
+
+
+
