92d1c66c0938aebf01e68169cb4d7bc66f79bd15
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-02-09 22:07:59 +0100

    1ère section

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <section class="banner-wrapper">
         <div class="empty"></div>
         <div class="banner">
-            <div class="banner-left">fiche de <br> renseignement</div>
+            <div class="banner-left"><h1>fiche de <br> renseignement</h1></div>
             <div class="banner-right">
                 <img src="./assets/img/IllusForm.png" alt="illustration bannière formulaire">
             </div>
@@ -30,7 +30,30 @@
         
     </section>
     <main>
-        <section></section>
+        <h2>Fiche de renseignement de l'étudiant</h2>
+        <section class="info-perso-wrapper">
+            <div class="info-perso-intro">
+                <div class="info-perso-title">Informations<span>Personnelles</span></div>
+                <div class="info-perso-img">
+                    <img src="./assets/img/infoPerso.png" alt="">
+                </div>
+            </div>
+            <div class="info-perso-form">
+                <form action="" method="GET" class="formulaire">
+                    <input type="text" placeholder="Nom" id="lastname">
+                    <input type="text" placeholder="Prénom" id="firstname">
+                    <input type="text" placeholder="Adresse" id="adress">
+                    <input type="text" placeholder="Code postal et ville" id="city">
+                    <input type="text" placeholder="Téléphone portable" id="mobile-phone">
+                    <input type="text" placeholder="Téléphone fixe" id="phone">
+                    <input type="text" placeholder="Adresse mail" id="email">
+                    <input type="text" placeholder="Date de naissance" id="birth-date">
+                    <input type="text" placeholder="Lieu de naissance" id="birth-city">
+                    <input type="text" placeholder="Pays de naissance" id="birth-country">
+                    <input type="text" placeholder="Nationalité" id="nationality">
+                </form>
+            </div>
+        </section>
     </main>
 </body>
 </html>
